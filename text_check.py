c996952eb037bcea25e1510945103158a8c1b1e6
laote <ihagiu1212@gmail.com>
2024-12-02 14:46:05 +0200

    Function load files was created.now from the main app we can use the dropdown button and select one of our files.A test file was created for this part

--- text_check.py
+++ text_check.py
@@ -9,6 +9,20 @@ from transformers import pipeline
 # pip install transformers (i comment this because i already installed this package)
 
 
+# Loading files in our app
+def load_selected_file():
+    global content
+    selected_file = file_dropdown.get()
+    if selected_file and selected_file != "Upload the file":
+        try:
+            file_path = os.path.join(os.getcwd(), selected_file)
+            with open(file_path, 'r', encoding='utf-8') as file:
+                content = file.read()
+            output_text.insert(tk.END, f"File succesfully uploaded: {selected_file}\nContain {len(content)} characters.\n")
+        except Exception as e:
+            output_text.insert(tk.END, f"Error when the file was uploaded: {str(e)}\n")
+    else:
+        output_text.insert(tk.END, "No file selected/ Invalid selection.\n")
 
 
 
@@ -16,13 +30,11 @@ from transformers import pipeline
 
 
 # User interface
-
 root = tk.Tk()
 root.title("Text checker using AI")
 
+
 # Dropdown menu for selecting files
-
-
 def list_text_files():
     current_directory = os.getcwd()
     return [file for file in os.listdir(current_directory) if file.endswith(".txt")]
@@ -41,19 +53,19 @@ load_button.pack(pady=10)
 
 
 # Question area
-
 question_label = tk.Label(root, text = "Write a question about your text file:")
 question_label.pack(pady=5)
 
+
 question_entry = tk.Entry(root, width=50)
 question_entry.pack(pady=5)
+
 
 ask_button = tk.Button(root, text ="Ask the question", command=None)
 ask_button.pack(pady=10)
 
 
-# Are where the answers are displayed
-
+# Area where the answers are displayed
 output_text = scrolledtext.ScrolledText(root, width=70, height=20)
 output_text.pack(pady=10)
 
