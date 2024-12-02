a17395e73932bf142025eb11185ba1a6a3237585
laote <ihagiu1212@gmail.com>
2024-12-02 13:46:23 +0200

    setting the env install some libs and import os, tkinter

---
+++ text_check.py
@@ -0,0 +1,9 @@
+# what you need to make this app functional
+# Import part
+import os
+import tkinter as tk
+from tkinter import filedialog, scrolledtext
+import re # we gonna use regex to create some patterns 
+from transformers import pipeline
+# Install required 
+# pip install transformers (i comment this because i already installed this package)
