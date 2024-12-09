757d4e524998cd6d7e6aa70c5f33119a2476f1fd
laote <ihagiu1212@gmail.com>
2024-12-09 13:09:55 +0200

    creating function for asking questions about the words lenght from one to 17 added the questions in handle_question().

--- text_check.py
+++ text_check.py
@@ -23,23 +23,134 @@ def count_characters(text):
     return len(text)
 
 # couting words by letters
+
+
 #1
-def word_with_one_letter(text):
+def words_with_one_letter(text):
     word = re.findall(r"\b\w{1}\b", text)
-    return len(word)
-#2
-def word_with_two_letter(text):
-    word = re.findall(r"\b\w{2}\b", text)
-    return len(word)
-#3
-def word_with_three_letter(text):
-    word = re.findall(r"\b\w{3}\b", text)
-    return len(word)
-#4
-def word_with_four_letter(text):
-    word = re.findall(r"\b\w{4}\b", text)
+    if not word:
+        return "Words with one character doesn t exists"
     return len(word)
 
+#2
+def words_with_two_letters(text):
+    word = re.findall(r"\b\w{2}\b", text)
+    if not word:
+        return "Words with two characters doesn t exists"
+    return len(word)
+
+#3
+def words_with_three_letters(text):
+    word = re.findall(r"\b\w{3}\b", text)
+    if not word:
+        return "Words with three characters doesn t exists"
+    return len(word)
+
+#4
+def words_with_four_letters(text):
+    word = re.findall(r"\b\w{4}\b", text)
+    if not word:
+        return "Words with four characters doesn t exists"
+    return len(word)
+
+#5
+def words_with_five_letters(text):
+    word = re.findall(r"\b\w{5}\b", text)
+    if not word:
+        return "Words with five characters doesn t exists"
+    else:
+        print("Print words with 5 letters:", word)
+        return len(word)
+
+#6
+def words_with_six_letters(text):
+    word = re.findall(r"\b\w{6}\b", text)
+    if not word:
+        return "Words with six characters doesn t exists"
+    return len(word)
+
+#7
+def words_with_seven_letters(text):
+    word = re.findall(r"\b\w{7}\b", text)
+    if not word:
+        return "Words with seven characters doesn t exists"
+    return len(word)
+
+#8
+def words_with_eight_letters(text):
+    word = re.findall(r"\b\w{8}\b", text)
+    if not word:
+        return "Words with eight characters doesn t exists"
+    return len(word)
+
+#9
+def words_with_nine_letters(text):
+    word = re.findall(r"\b\w{9}\b", text)
+    if not word:
+        return "Words with nine characters doesn t exists"
+    return len(word)
+
+#10
+def words_with_ten_letters(text):
+    word = re.findall(r"\b\w{10}\b", text)
+    if not word:
+        return "Words with ten characters doesn t exists"
+    return len(word)
+
+#11
+def words_with_eleven_letters(text):
+    word = re.findall(r"\b\w{11}\b", text)
+    if not word:
+        return "Words with eleven characters doesn t exists"
+    return len(word)
+
+#12
+def words_with_twelve_letters(text):
+    word = re.findall(r"\b\w{12}\b", text)
+    if not word:
+        return "Words with twelve characters doesn t exists"
+    return len(word)
+
+#13
+def words_with_thirteen_letters(text):
+    word = re.findall(r"\b\w{13}\b", text)
+    if not word:
+        return "Words with thirteen characters doesn t exists"
+    return len(word)
+
+
+
+#14
+def words_with_fourteen_letters(text):
+    word = re.findall(r"\b\w{14}\b", text)
+    if not word:
+        return "Words with fourteen characters doesn t exists"
+    return len(word)
+
+
+
+#15
+def words_with_fifteen_letters(text):
+    word = re.findall(r"\b\w{15}\b", text)
+    if not word:
+        return "Words with fifteen characters doesn t exists"
+    return len(word)
+
+#16
+def words_with_sixteen_letters(text):
+    word = re.findall(r"\b\w{16}\b", text)
+    if not word:
+        return "Words with sixteen characters doesn t exists"
+    else:
+        print("Words with 16 letters:", word)
+        return len(word)
+
+#17
+def words_with_seventeen_letters(text):
+    word = re.findall(r"\b\w{17}\b", text)
+    if not word:
+        return "Words with seventeen characters doesn t exists"
+    return len(word)
 
 
 # function to handle the user questions
@@ -51,14 +162,69 @@ def handle_question(question, context):
         return f"The file contains {count_punctuation(content)} punctuation signs"
     elif "how many characters" in question.lower():
         return f"The file contains {count_characters(content)} characters"
-    elif "words with one letter" in question.lower():
-        return f"The file contains {word_with_one_letter(content)} words with one letter"
-    elif "words with two letter" in question.lower():
-        return f"The file contains {word_with_two_letter(content)} words with two letter"
-    elif "words with three letter" in question.lower():
-        return f"The file contains {word_with_three_letter(content)} words with three letter"
-    elif "words with four letter" in question.lower():
-        return f"The file contains {word_with_four_letter(content)} words with four letter"
+    #1
+    elif "words with 1" in question.lower():
+        return f"The file contains {words_with_one_letter(content)} words with one letter"
+    #2
+    elif "words with 2" in question.lower():
+        return f"The file contains {words_with_two_letters(content)} words with two letters"
+    #3
+    elif "words with 3" in question.lower():
+        return f"The file contains {words_with_three_letters(content)} words with three letters"
+    #4
+    elif "words with 4" in question.lower():
+        return f"The file contains {words_with_four_letters(content)} words with four letters"
+    #5
+    elif "words with 5" in question.lower():
+        return f"The file contains {words_with_five_letters(content)} words with five letters"
+
+    #6
+    elif "words with 6" in question.lower():
+        return f"The file contains {words_with_six_letters(content)} words with six letters"
+
+    #7
+    elif "words with 7" in question.lower():
+        return f"The file contains {words_with_seven_letters(content)} words with seven letters"
+
+    #8
+    elif "words with 8" in question.lower():
+        return f"The file contains {words_with_eight_letters(content)} words with eight letters"
+
+    #9
+    elif "words with 9" in question.lower():
+        return f"The file contains {words_with_nine_letters(content)} words with nine letters"
+
+    #10
+    elif "words with 10" in question.lower():
+        return f"The file contains {words_with_ten_letters(content)} words with ten letters"
+
+    #11
+    elif "words with 11" in question.lower():
+        return f"The file contains {words_with_eleven_letters(content)} words with eleven letters"
+
+    #12
+    elif "words with 12" in question.lower():
+        return f"The file contains {words_with_twelve_letters(content)} words with twelve letters"
+
+    #13
+    elif "words with 13" in question.lower():
+        return f"The file contains {words_with_thirteen_letters(content)} words with thirteen letters"
+
+    #14
+    elif "words with 14" in question.lower():
+        return f"The file contains {words_with_fourteen_letters(content)} words with fourteen letters"
+
+    #15
+    elif "words with 15" in question.lower():
+        return f"The file contains {words_with_fifteen_letters(content)} words with fifteen letters"
+
+    #16
+    elif "words with 16" in question.lower():
+        return f"The file contains {words_with_sixteen_letters(content)} words with sixteen letters"
+
+    #17
+    elif "words with 17" in question.lower():
+        return f"The file contains {words_with_seventeen_letters(content)} words with seventeen letters"
 
     else:
         qa_pipeline = pipeline("question-answering", model='distilbert-base-uncased-distilbert-squad')
